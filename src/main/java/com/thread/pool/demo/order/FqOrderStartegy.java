dd475bc4895a4c9394c41c8a542eddb9e34c2b1a
bo.zhang2 <zhangbo@sijibao.com>
2019-10-25 15:23:58 +0800

    11111

--- src/main/java/com/thread/pool/demo/order/FqOrderStartegy.java
+++ src/main/java/com/thread/pool/demo/order/FqOrderStartegy.java
@@ -4,6 +4,7 @@ public abstract class FqOrderStartegy implements AbstractOrderStartegy {
 
     public String addOrder(String orderNo){
         System.out.println("111111111111111111111111111111");
+        System.out.println("111111111");
         return "orderNo";
     }
 }
